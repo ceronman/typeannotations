14931955b7942238d212401ae6509bc04f92694d
Hong Minhee <minhee@dahlia.kr>
2014-08-20 01:53:01 +0900

    Packaging using distutils

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+setup(
+    name='typeannotations',
+    version='1.0.0',
+    description='A library with a set of tools for annotating types '
+                'in Python code.',
+    url='https://github.com/ceronman/typeannotations',
+    author='Manuel Cerón',
+    author_email='ceronman' '@' 'gmail.com',
+    license='Apache License',
+    packages=['annotation'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
